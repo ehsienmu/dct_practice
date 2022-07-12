62d079de6a455bbcdb2f6178f2f02357acfb9f57
hsien <43429849+ehsienmu@users.noreply.github.com>
2022-07-12 23:26:15 +0800

    Delete testdct.py

--- testdct.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch_dct as dct
-
-x = torch.randn(200)
-X = dct.dct(x)   # DCT-II done through the last dimension
-print('X',X.size())
-y = dct.idct(X)  # scaled DCT-III done through the last dimension
-print ((torch.abs(x - y)).sum())  # x == y within numerical tolerance
-# assert (torch.abs(x - y)).sum() < 1e-10  # x == y within numerical tolerance
